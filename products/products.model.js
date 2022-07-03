4defa979da88991d67ac581447b3af4ab5ccdfae
Mihir Solankar <mihir.solankar1@gmail.com>
2022-07-03 18:49:05 +0530

    Added Mutations

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
     id: "redshoe",
     description: "Red Shoe",
     price: 779.99,
+    reviews: [],
   },
   {
     id: "bluejean",
     description: "Blue Jeans",
     price: 699.0,
+    reviews: [],
   },
 ];
 
@@ -15,4 +17,34 @@ function getAllProducts() {
   return products;
 }
 
-module.exports = { getAllProducts };
+function getProductsByPrice(min, max) {
+  return products.filter((x) => {
+    return x.price >= min && x.price <= max;
+  });
+}
+
+function getProductByID(id) {
+  return products.find((x) => {
+    return product.id === id;
+  });
+}
+
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: [],
+  };
+
+  products.push(newProduct);
+
+  return newProduct;
+}
+
+module.exports = {
+  getAllProducts,
+  getProductsByPrice,
+  getProductByID,
+  addNewProduct
+};
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -5,5 +5,16 @@ module.exports = {
     products: () => {
       return productsModel.getAllProducts();
     },
+    productsByPrice: (_, args) => {
+      return productsModel.getProductsByPrice(args.min, args.max);
+    },
+    productsByID: (_, args) => {
+      return productsModel.getProductByID(args.id);
+    },
+  },
+  Mutation: {
+    addNewProduct: (_, args) => {
+      productsModel.addNewProduct(args.id, args.description, args.price);
+    },
   },
 };
